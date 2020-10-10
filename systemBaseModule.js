9f846233500d2a265d1f338e7d37eb77ec8cc72a
Sangeeth Nandakumar <sangeethnandakumar@gmail.com>
2020-10-10 12:38:20 +0530

    Update systemBaseModule.js

--- systemBaseModule.js
+++ systemBaseModule.js
@@ -1,19 +1,51 @@
-var systemBaseModule = (function () {
+//Generic functions
 
-    //APIs exposed
-    return {
-        playMusic: play,
-        pauseMusic: pause,
-        showUpNext: showNextTrack,
-        addTrack: addTrackToMusicQueue
+//DEBOUNCING - Execute every X ms after previous event
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+//THROTTLING - Execute every X ms without checking previous event
+function throttle(callback, limit) {
+    var waiting = false;
+    return function () {
+        if (!waiting) {
+            callback.apply(this, arguments);
+            waiting = true;
+            setTimeout(function () {
+                waiting = false;
+            }, limit);
+        }
     }
+}
 
-    //Public functions
-
-
-    //Private functions
-    function play() {
-        console.log('Im playing the next song!');
+//STANDARD ERROR HANDLING
+function handleErrorMessage(standardResponse) {
+    if (standardResponse != null) {
+        switch (standardResponse.responseStatus ) {
+            case 1:
+                //Success
+                break;
+            case 2:
+                //Warning
+                break;
+            case 3:
+                //Error
+                break;
+                //Exception
+            case 4:
+                break
+        }
     }
-   
-})();
+}
